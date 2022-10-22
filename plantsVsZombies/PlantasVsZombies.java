52412a68938e3d889e5619d4e504949f955cd108
Lucía Alvarez <58492690+Luci435@users.noreply.github.com>
2022-10-22 18:24:39 -0300

    Add files via upload

--- plantsVsZombies/PlantasVsZombies.java
+++ plantsVsZombies/PlantasVsZombies.java
@@ -12,12 +12,13 @@ public class PlantasVsZombies {
         Scanner read = new Scanner(System.in);
         
         System.out.println("Bienvenido a Plantas vs Zombies");
-        System.out.println("Si desea jugar presione 1");
-        System.out.println("Si desea ver 50 movimientos presione 2");
+        
         
         boolean flag=true;
         
         while (flag){
+            System.out.println("Si desea jugar presione 1");
+            System.out.println("Si desea ver 50 movimientos presione 2");
             opcion = read.nextLine();
             try {
                 int opcionInt=Integer.parseInt(opcion);
@@ -29,15 +30,11 @@ public class PlantasVsZombies {
                         flag=false;
                         break;
                     default: System.out.println("No eligió una opción correcta");
-                    System.out.println("Si desea jugar presione 1");
-                    System.out.println("Si desea ver 50 movimientos presione 2");
                         break;
                 }
             }
             catch (NumberFormatException e){
                 System.out.println("Debe ingresar un número");
-                System.out.println("Si desea jugar presione 1");
-                System.out.println("Si desea ver 50 movimientos presione 2");
             }
         }
         
