acc6001f72928346189f5ef07fc2778c30df8fbb
Lucía Alvarez <58492690+Luci435@users.noreply.github.com>
2022-10-22 19:24:25 -0300

    Add files via upload

--- plantsVsZombies/Petacereza.java
+++ plantsVsZombies/Petacereza.java
@@ -2,15 +2,13 @@
 • Petacereza: Provoca una explosión en un área de 3x3 capaz de matar a casi cualquier zombi. Tiene
 un coste de 150 soles.
  */
-package plantsVsZombies;
+package plantasvszombies;
 
-/**
- *
- * @author cocuc
- */
 public class Petacereza extends Planta {
     public Petacereza(int x, int y){
         super(x,y);
+        //rango
+        this.danio=150;
     }
     
 }
