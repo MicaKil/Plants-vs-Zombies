50086e10ae6fea2cc667fec4ba23c41afa0d8a0e
Lucía Alvarez <58492690+Luci435@users.noreply.github.com>
2022-10-29 18:40:15 -0300

    No se como borrar :c

--- plantsVsZombies/Guisantralladora.java
+++ plantsVsZombies/Guisantralladora.java
@@ -1,16 +1,1 @@
-/*
-• Guisantralladora: Dispara cuatro guisantes a la vez. Para plantarla en el patio se necesita estar
-plantada sobre una Repetidora. Su coste es de 250 soles.
- */
-package plantsVsZombies;
 
-//poner como subclase pq necesita al lanzaguisantes para plantar
-public class Guisantralladora extends Planta {
-    //protected boolean predecesor; //plantear bien esta exigencia
-    public Guisantralladora(int x, int y){
-        super(x,y);
-        this.danio=100;
-        this.costo=250;
-        this.id ='U';
-    }
-}
