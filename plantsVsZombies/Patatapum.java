e8ae1e80cd36c1a0eba3315c1779a831b684156d
Lucía Alvarez <58492690+Luci435@users.noreply.github.com>
2022-11-02 19:26:00 -0300

    Update Patatapum.java

--- plantsVsZombies/Patatapum.java
+++ plantsVsZombies/Patatapum.java
@@ -13,25 +13,27 @@ public class Patatapum extends Planta {
         this.danio=75; 
         this.id='P';
     }
-    
+    /*el primer turno con zombie delante no hace nada, el segundo turno explota*/
     @Override
     public void atacar(Planta p, PlantsVsZombies juego){
         Tablero t = juego.tablero;
         int i = p.getX();
         int j=p.getY() ;
         if (this.explotar){
-            //lo elimino del tablero para no tener problemas con los char
+            //se elimina del tablero 
             t.tableroP[i][j]=null;
             juego.setCantPatatapum(juego.getCantPatatapum() - 1); // se resta en uno la cantidad actual
-
+            //hace daño al zombie de la posición j+1 de la misma linea (en frente/a la derecha)
             int vidaActual = t.tableroZ[i][j + 1].getVida() - this.danio;
             t.tableroZ[i][j + 1].setVida(vidaActual);
             System.out.printf("- El patatapum en la posición (%d,%d) le hizo %d de daño a: '%s' en la posición (%d,%d).\n",
                     i + 1, j + 1, this.danio, t.tableroZ[i][j + 1].getId(), i + 1, j + 2);
+            //si muere el zombie avisa
             if (t.tableroZ[i][j + 1].vida <= 0) {
                 System.out.println("  - El zombie '" + t.tableroZ[i][j + 1].getId() + "' ha muerto x_x");
                 t.tableroZ[i][j + 1] = null;
             } else {
+                //detalles de caracono y caracubo
                 t.tableroZ[i][j + 1].setAtacando(false); // deja de atacar
                 if (t.tableroZ[i][j + 1].getId() == 'c' && vidaActual <= 100) {
                     t.tableroZ[i][j + 1].setId('z');
